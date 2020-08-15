b0e3735bb702a3fe8554c214e4e0c1cd86289797
Maksym Prokopov <mprokopov@gmail.com>
2020-08-15 16:03:25 +0200

    fixed test name

--- test/privat_manager/manager/api_test.clj
+++ test/privat_manager/manager/api_test.clj
@@ -1,16 +1,17 @@
-(ns privat-manager.privat.util-test
+(ns privat-manager.manager.api-test
   (:require 
-            [privat-manager.manager.api :as manager.api]
+            [privat-manager.manager.api :refer :all]
+            [clojure.test :refer :all]
             ))
 
 (deftest test-header-location
   (is
    (=
     "f1a582f4-537b-4159-82ff-5d1d76e88918"
-    (manager.api/parse-header-location 
+    (parse-header-location 
      "/api/SVQtUHJlbWl1bQ/f1a582f4-537b-4159-82ff-5d1d76e88918.json"))))
 
 (deftest test-paymant-view-link
   (is
    (= "http://192.168.180.31:8080/receipt-or-payment-view?Key=0a89cc07-dce0-4f49-b35f-865daee3c024&FileID=SVQtUHJlbWl1bQ"
-      (manager.api/payment-view-link "0a89cc07-dce0-4f49-b35f-865daee3c024" "SVQtUHJlbWl1bQ"))))
+      (payment-view-link "0a89cc07-dce0-4f49-b35f-865daee3c024" "SVQtUHJlbWl1bQ"))))
